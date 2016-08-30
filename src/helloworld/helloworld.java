543d10bf84652ffe96b7c8f51ba8b5bc50d2c651
deser <deser@65.126.174.18>
2016-08-30 12:09:27 -0600

    basic hello version

---
+++ src/helloworld/helloworld.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author deser
+ */
+
+import static kiss.API.*;
+public class helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+        void UNtestRun() {
+            outExpect("Hello!");
+            run();
+            outClose();
+        }
+            
+         void run(){
+            println("Hello!");
+        }
+}
