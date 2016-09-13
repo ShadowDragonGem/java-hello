514cd2a7dd3dd78a331021c33337789ca4c68ee7
deser <deser@65.126.174.18>
2016-09-13 11:12:45 -0600

    test int

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -21,48 +21,112 @@ public class helloworld {
             {run();}
         }
             
-         void run(){
+        void run(){
             println("Hello!"); 
         }
          
-         void testBooleans()
-         {
-             assert true !=false;
-             assert true == true;
-             assert false == false;
-             try(Close out = outExpect("true")) {println(true);}
-             try(Close out = outExpect("false")) {println(false);}
+        void testBooleans()
+        {
+            assert true !=false;
+            assert true == true;
+            assert false == false;
+            try(Close out = outExpect("true")) {println(true);}
+            try(Close out = outExpect("false")) {println(false);}
 
-             assert !true== false;
-             assert !false == true;
+            assert !true== false;
+            assert !false == true;
+            
+            assert (true && true) == true;
+            assert (true && false) == false;
+            assert (false && true) == false;
+            assert (false && false) == false;
              
-             assert (true && true) == true;
-             assert (true && false) == false;
-             assert (false && true) == false;
-             assert (false && false) == false;
+            assert (true || true) == true;
+            assert (true || false) == true;
+            assert (false || true) == true;
+            assert (false || false) == false;
+        }
+         
+        int side;
+        void sideEffect(int y){
+            println(y);
              
-             assert (true || true) == true;
-             assert (true || false) == true;
-             assert (false || true) == true;
-             assert (false || false) == false;
-         }
+        }
          
          void testInts(){
-             assert Integer.MAX_VALUE ==Math.pow(2, 31)-1;
-             assert Integer.MIN_VALUE ==-Math.pow(2, 31);
-             assert Integer.MIN_VALUE - 1 == Integer.MAX_VALUE;
-             assert Integer.MAX_VALUE +1 == Integer.MIN_VALUE;
+            assert Integer.MAX_VALUE ==Math.pow(2, 31)-1;
+            println(Integer.MAX_VALUE);
+            assert Integer.MAX_VALUE == 2147483647;
+            
+            assert Integer.MIN_VALUE ==-Math.pow(2, 31);
+            println(Integer.MIN_VALUE);
+            assert Integer.MIN_VALUE == -2147483648;
+            assert Integer.MIN_VALUE == 0b1000_0000_0000_0000_0000_0000_0000_0000;
              
-             int x = 3;
-             int y = 0xffff;
-             int z = 0b10_01;
+            assert Integer.MIN_VALUE - 1 == Integer.MAX_VALUE;
+            assert Integer.MAX_VALUE +1 == Integer.MIN_VALUE;
              
-             assert x==3;
-             assert y ==65_535;
-             assert z == 9;
+            int x = 3;
+            int y = 0xffff;
+            int z = 0b10_01;
+            
+            assert x==3;
+            assert y ==65_535;
+            assert z == 9;
              
-             assert Integer.toString(x, 10).equals("3");
-             assert Integer.toString(y, 16).equals("ffff");
-             assert Integer.toString(z, 2).equals("1001");
-         }
+            assert Integer.toString(x, 10).equals("3");
+            assert Integer.toString(y, 16).equals("ffff");
+            assert Integer.toString(z, 2).equals("1001");
+             
+            int a=1;
+             
+            ++a;
+             
+            try(Close out = outExpect("2")) { println(a);}
+            try(Close out = outExpect("3")) {println(++a);}
+            try(Close out = outExpect("3")) {println(a);}
+            
+            //bitwise not
+            assert ~0b0000_0000_0000_0000_0000_0000_1111_0000
+                 == 0b1111_1111_1111_1111_1111_1111_0000_1111;
+            
+            //bitwise and
+            assert (0b1111_0000
+                  & 0b1010_1010) ==
+                    0b1010_0000;
+            
+            //bitwise or
+            assert (0b1111_0000
+                  | 0b1010_1010) ==
+                    0b1111_1010;
+            
+            //bitshift operator
+            assert(0b1010_1111_0000 >> 4) == 
+                        0b1010_1111;
+            
+            int b = random(0, 1_000_000);
+            assert((b>>1)== b/2);
+            assert(((-b)>>1)==-b/2);
+            assert((b<<1)==b*2);
+            assert(((-b)<<1)==2*(-b));
+            
+            int c = random(-1000000, 1000000);
+            assert ((~c)+1) == -c;
+            
+            assert-2 == 0b1111_1111_1111_1111_1111_1111_1111_1110;
+            
+            assert (-2>>1) == 0b1111_1111_1111_1111_1111_1111_1111_1111;
+            println(Integer.toString(-2,2));
+        }
+         
+        void testConvert(){
+            byte x = -1;
+            assert x == (byte)0b1111_1111;
+            int y = x;
+            assert y == -1;
+            assert 0b1111_1111_1111_1111_1111_1111_1111_1111 ==
+                                    (int) (byte) 0b1111_1111;
+        }
+        
+        
 }
