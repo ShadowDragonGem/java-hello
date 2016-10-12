a8e5dfa1ec58b810c5eb46d444732f0d0592be3f
deser <deser@65.126.174.18>
2016-10-12 16:52:41 -0600

    First test, and printing number of unique pairs

--- src/collectionHomework/CollectionHomework.java
+++ src/collectionHomework/CollectionHomework.java
@@ -4,15 +4,33 @@
  * and open the template in the editor.
  */
 package collectionHomework;
-import java.util.Scanner;
+import java.util.*;
 import static kiss.API.*;
 
 /**
  *
- * @author deser
+ * @author Sarah Harvey
  */
 public class CollectionHomework {
+    /*
+    Problem on hakerRank
+    https://www.hackerrank.com/challenges/java-hashset
+    plus some more things to make it more interesting
+    */
     
+    void testPairsUnique(){
+        String test_left1 = "rachel";
+        String test_right1 = "tim";
+        String test_left2 = "sam";
+        String test_right2 = "dee";
+        
+        HashSet<String> pairs = new HashSet<String>();
+        
+        pairs.add("(" + test_left1 + ", " + test_right1 + ")");
+        pairs.add("(" + test_left2 + ", " + test_right2 + ")");
+        println(pairs);
+
+    }
     
     void run() {
         Scanner s = new Scanner(System.in);
@@ -23,6 +41,13 @@ public class CollectionHomework {
         for(int i = 0; i<t; i++){
             pair_left[i] = s.next();
             pair_right[i] = s.next();
+        }
+        
+        HashSet<String> pairs = new HashSet<String>();
+        
+        for(int i = 0; i<t; i++){
+            pairs.add("(" + pair_left[i] + ", " + pair_right[i] + ")");
+            println(pairs.size());
         }
     }
 }
